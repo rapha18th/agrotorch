4bcd84e64959e24651510c7a49ee92c6e2fb2151
rapha18th <rairorr@gmail.com>
2020-08-29 18:18:11 +0200

    test responses

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def verify_fb_token(token_sent):
 
 
 def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you",
-                        "Keep on being you!", "We're greatful to know you :)"]
+    sample_responses = ["You are so cool", "We're proud of you",
+                        "Keep on being cool!", "We're greatful to know you :)"]
     # return selected item to the user
     return random.choice(sample_responses)
 
