f45d5b55c60cb98db415ff8f78201a53897e8b48
rapha18th <rairorr@gmail.com>
2020-09-08 17:19:58 +0200

    i

--- app.py
+++ app.py
@@ -43,8 +43,9 @@ def receive_message():
                 if message.get('message'):
                     # Facebook Messenger ID for user so we know where to send response back to
                     recipient_id = message['sender']['id']
+                    x = message["message"]["text"].lower()
                     if message['message'].get('text'):
-                        x = output["entry"][0]["messaging"][0]["message"]["text"].lower()
+                       
                         quick_response(recipient_id,
                             "Hi my name is Agrotorch and I'm here to help, to start you can upload an image of diseased plant or choose what you want to learn below ",
                                 'maize', 'soybean', 'potato', 'tomato', postcard1="maize", postcard2="soybean", postcard3="potato", postcard4="tomato")
