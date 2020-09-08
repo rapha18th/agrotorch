617587db0bc874b5b54359d78c7e11967fd4f881
rapha18th <rairorr@gmail.com>
2020-09-08 15:43:50 +0200

    i

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def receive_message():
                     if message['message'].get('text'):
                         quick_reply_message = "Hi my name is Agrotorch and I'm here to help, to start you can upload an image of diseased plant or choose what you want to learn below "
                         quick_rep_option = (["maize","stark_payload"],["soybean","lan_payload"],["potato","tar_payload"],["tomato","None"])
-                        send_quickreply(recipient_id,quick_reply_message,quick_rep_option)
+                        bot.send_quickreply(recipient_id,quick_reply_message,quick_rep_option)
 
                     elif 'maize' == quick_rep_option:
                         send_video_url(recipient_id, "https://youtu.be/AwkXRwCPHI0")
